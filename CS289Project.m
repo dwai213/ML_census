66922ca8607367ecbaad639439e95086d456ab0c
SonyChiang <sony@SonytekiMacBook-Pro.local>
2015-04-03 23:10:12 -0700

    Produce train data and test data

--- CS289Project.m
+++ CS289Project.m
@@ -4,5 +4,6 @@ TrainFile = 'adult_data';
 TestFile = 'adult_test';
 [Train,TrainLabel] = ImportFile(TrainFile);
 [Test, TestLabel] = ImportFile(TestFile);
-save('Train_data.mat','Train','TrainLabel');
-save('Test_data.mat','Test','TestLabel');
+save('ML_census/Train_data.mat','Train','TrainLabel');
+save('ML_census/Tclest_data.mat','Test','TestLabel');
+
