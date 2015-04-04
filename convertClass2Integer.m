8a5ff255c5ea53307647c8903dd620aee17a6138
dwai213 <dwai213@gmail.com>
2015-04-03 21:11:13 -0700

    Imported Data now returns a structure of Doubles instead of cell arrays

--- convertClass2Integer.m
+++ convertClass2Integer.m
@@ -2,9 +2,9 @@ function convertedData = convertClass2Integer(raw_data,table)
 %This function converts the String feature in raw_data and
 %uses the provided table to convert it to an integer
 
-convertedData = cell(size(raw_data));
+convertedData = zeros(size(raw_data));
 for i = 1:length(raw_data)
-  convertedData{i} = table(raw_data{i});
+  convertedData(i) = table(raw_data{i});
 end
 
 end
