ca3ab3c9d32234aa8d90f978082233838aa3d2e7
dwai213 <dwai213@gmail.com>
2015-04-20 16:51:45 -0700

    updated naiveNN to reflect new processed data

--- naiveNN.m
+++ naiveNN.m
@@ -1,27 +1,28 @@
 %% Import data
+cd /Users/dwai/Documents/Box/MATLAB/Graduate/CS289A/ML_census
 [Data Label] = ImportFile('adult_data');
 
 %% Partition to test and training data
-test_data = [Data.AGE(1:6500) Data.HOURS_PER_WEEK(1:6500)];
+test_data = [Data(1:6500,1) Data(1:6500,13)];
 test_label = Label(1:6500);
-training_data = [Data.AGE(6501:end) Data.HOURS_PER_WEEK(6501:end)];
+training_data = [Data(6501:end,1) Data(6501:end,13)];
 training_label = Label(6501:end);
 
 %% Plot Training Data
 
-figure(2), hold on
-for i = 1:length(training_label)
-  prediction = training_label(i);
-  x = training_data(i,1);
-  y = training_data(i,2);
-  if prediction == 1
-    plot(x,y,'go')
-  else
-    plot(x,y,'mo')
-  end
-end
-title('Training Data and their Labels')
-xlabel('Age'), ylabel('Hours per Week'), axis([10 90 0 100])
+% figure(2), hold on
+% for i = 1:length(training_label)
+%   prediction = training_label(i);
+%   x = training_data(i,1);
+%   y = training_data(i,2);
+%   if prediction == 1
+%     h_1 = plot(x,y,'go');
+%   else
+%     h_0 = plot(x,y,'mo');
+%   end
+% end
+% title('Training Data and their Labels')
+% xlabel('Age'), ylabel('Hours per Week'), axis([10 90 0 100])
 
 %% Train a kNN Model
 
@@ -37,7 +38,7 @@ xy = [x(:) y(:)];
 meshLabels = predict(mdl,xy);
 decisionBoundaries = reshape(meshLabels,size(x));
 figure(1), clf, hold on
-imagesc(xrange,yrange,decisionBoundaries)
+h_img = imagesc(xrange,yrange,decisionBoundaries);
 cmap = [166 136 163; 0 73 27]./255;
 colormap(cmap);
 
@@ -46,13 +47,14 @@ for i = 1:length(predictions)
   x = test_data(i,1);
   y = test_data(i,2);
   if prediction == 1
-    plot(x,y,'go')
+    h_1 = plot(x,y,'go');
   else
-    plot(x,y,'mo')
+    h_0 = plot(x,y,'mo');
   end
 end
-title('Test Data Superimposed on Decision Boundary') 
+title('kNN Classifier, k =3, Manhattan distance metric') 
 xlabel('Age'), ylabel('Hours per Week'), axis([10 90 0 100])
+legend([h_1;h_0],'Income >50k','Income <=50k')
 
 %% Determine Error Rate
 incorrect = 0;
